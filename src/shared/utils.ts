17e38c7fe45794e3e317eb23262a15b6b4af9518
Tanel Metsar <tanel.metsar@cgi.com>
2020-07-08 19:06:51 +0300

    WE2-84 [doc] Doc blocks for util functions

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,11 +1,40 @@
 import TypedMap from "../models/TypedMap";
 
+/**
+ * Encode an array of bytes to a Base64 encoded string.
+ *
+ * @param byteArray Array of bytes to encode
+ *
+ * @returns The Base64 encoded string
+ *
+ * @example
+ *   toBase64([72, 101, 108, 111])
+ *   // =>  "SGVsbG8="
+ */
 export function toBase64(byteArray: number[]): string {
   return btoa(
     byteArray.reduce((acc, curr) => acc += String.fromCharCode(curr), "")
   );
 }
 
+/**
+ * Transforms the Fetch API Header object to plain JSON.stringify-able object type.
+ *
+ * @param headers Fetch API Headers object
+ *
+ * @returns The headers in a simple object, where keys and values are strings.
+ *
+ * @example
+ *   headersToObject(fetchResponse.headers)
+ *   // => {
+ *   //   "connection":     "keep-alive",
+ *   //   "content-length": "49",
+ *   //   "content-type":   "application/json; charset=utf-8",
+ *   //   "date":           "Mon, 27 Apr 2020 06:28:54 GMT",
+ *   //   "etag":           "W/\"30-YHV2nUGU912eoDvI+roJ2Yqn5SA\"",
+ *   //   "x-powered-by":   "Express"
+ *   // }
+ */
 export function headersToObject(headers: Headers): TypedMap<string> {
   function reducer(acc: TypedMap<string>, curr: Array<string>): TypedMap<string> {
     if (typeof curr[0] == "string") {
@@ -26,7 +55,7 @@ export function headersToObject(headers: Headers): TypedMap<string> {
  * @param object Object to pick from
  * @param keys   Keys to pick from the object
  *
- * @returns The new object.
+ * @returns The new object
  *
  * @example
  *   const object = { "a": 1, "b": 2, "c": 3 };
